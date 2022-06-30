071ac542af52a8a770a41083f179fa89e1e14e8b
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-01 00:13:00 +0600

    client side data save to server side into database

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express')
 const app = express();
 const port = process.env.port || 5000;
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res)=>{
     res.send('server app testing success')
 });
+
+const uri = "mongodb+srv://users:4p0QpTLyFR33djF0@cluster0.kwdbbxh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        await client.connect();
+        const usersCollection = client.db("practice").collection("users");
+        // post or add new user
+        app.post('/user', async(req, res)=>{
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+    }finally{
+        // await client.close(); // after get some result need to close then use close
+
+    }
+
+}
+run().catch(console.dir);
 
 app.listen(port, ()=>{
     console.log(`example listining on ${port}`, );
